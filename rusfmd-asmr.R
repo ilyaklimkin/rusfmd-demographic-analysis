817229a29e9457bc51993c625d847d91c80e677d
Ilya Klimkin <klimkin695@gmail.com>
2021-02-25 13:21:15 +0300

    Update rusfmd-asmr.R

--- rusfmd-asmr.R
+++ rusfmd-asmr.R
@@ -33,8 +33,7 @@ dr <- dr %>%
 # set the age standard (European Population Standard 1976 by default)
 Age <- unique(dr$Age)
 Weight <- c(0.016,0.064,0.07,0.07,0.07,0.07,0.07,0.07,0.07,0.07,0.07,0.07,0.06,0.05,0.04,0.03,0.02,0.01,0.01) # set the vector of weights
-ESP <- as.data.frame(cbind(Age,Weight)) # merge the vector of age groups and the vector population weights
-ESP$Weight <- as.numeric(ESP$Weight) # transform weights into numeric format
+ESP <- tibble(Age,Weight) # merge the vector of age groups and the vector population weights into tibble
 
 # join population weights with mortality data
 dr <- left_join (dr, ESP)
