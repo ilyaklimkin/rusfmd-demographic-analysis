84c72cff2317dedd657fcbd42856f65f1d06b452
ilyaklimkin <klimkin695@gmail.com>
2021-11-22 00:57:47 +0300

    Git check

--- rusfmd-asmr.R
+++ rusfmd-asmr.R
@@ -6,6 +6,8 @@
 # (data downloaded on [22-10-2020]).
 # ==============================================================================
 
+# NEW LINE
+
 # set working directory
 setwd ("C:/Users/Ilya/Google Drive/R Projects/github/rusfmd-demographic-analysis") # <- set YOUR working directory
 getwd ()
